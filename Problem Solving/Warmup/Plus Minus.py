2a7c91a37e032bd27cabcc6df1d8032d68d14877
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-08-25 18:38:24 +0530

    Create Plus Minus.py

---
+++ Problem Solving/Warmup/Plus Minus.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the plusMinus function below.
+def plusMinus(arr):
+    pos , neg , zero =0 , 0 , 0
+    for i in arr:
+        if i>0:
+            pos = pos + 1
+        elif i<0:
+            neg = neg + 1
+        else:
+            zero+=1
+
+    print('%.6f'%(pos/len(arr)))
+    print('%.6f'%(neg/len(arr)))
+    print('%.6f'%(zero/len(arr)))
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
