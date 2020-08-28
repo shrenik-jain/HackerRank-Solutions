9bac565ea1f08a5035546ac494129dabd1a888e3
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-08-28 17:53:26 +0530

    Create Jumping on the Clouds.py

---
+++ Problem Solving/Implementation/Jumping on the Clouds.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+    length = len(c)
+    steps , i = 0 , 0
+    while i<length-1:
+        if i < length-2 and c[i+2] == 0: 
+            steps = steps + 1
+            i = i + 2
+        else:
+            steps = steps + 1
+            i=i+1 
+
+    return steps
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
