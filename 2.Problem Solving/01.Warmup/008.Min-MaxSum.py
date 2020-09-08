31dc07d7df26e61b192225a4ddf17eab683723d5
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-08 18:35:07 +0530

    Update 008.Min-MaxSum.py

--- 2.Problem Solving/01.Warmup/008.Min-MaxSum.py
+++ 2.Problem Solving/01.Warmup/008.Min-MaxSum.py
@@ -1,5 +1,7 @@
 '''
-Question : Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+Question : Given five positive integers, find the minimum and maximum values that can be calculated by summing 
+           exactly four of the five integers. 
+           Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
 Link : https://www.hackerrank.com/challenges/mini-max-sum/problem
 '''
