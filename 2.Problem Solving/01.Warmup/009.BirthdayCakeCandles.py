0d5e633e93567c0ff9eb6c65f491fcc7c349bcae
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-08 18:36:43 +0530

    Update 009.BirthdayCakeCandles.py

--- 2.Problem Solving/01.Warmup/009.BirthdayCakeCandles.py
+++ 2.Problem Solving/01.Warmup/009.BirthdayCakeCandles.py
@@ -10,14 +10,11 @@ def birthdayCakeCandles(ar):
     return ar.count(max(ar))
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     ar_count = int(input())
 
     ar = list(map(int, input().rstrip().split()))
 
     result = birthdayCakeCandles(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+           
+    print(result)
