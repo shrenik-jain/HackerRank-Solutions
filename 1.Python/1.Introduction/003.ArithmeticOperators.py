4b953737089e14d11b0da80f009fa2f55ec85f38
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-08 18:15:28 +0530

    Update and rename Arithmetic Operators.py to 003.ArithmeticOperators.py

---
+++ 1.Python/1.Introduction/003.ArithmeticOperators.py
@@ -0,0 +1,14 @@
+'''
+Question : 1.The first line contains the sum of the two numbers.
+           2.The second line contains the difference of the two numbers (first - second).
+           3.The third line contains the product of the two numbers.
+
+Link : https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
