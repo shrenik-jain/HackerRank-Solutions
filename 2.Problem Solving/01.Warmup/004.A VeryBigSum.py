338efd62c5cf7eb2c35a145fb1d243794a2f24f8
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-08 18:33:07 +0530

    Update 004.A VeryBigSum.py

--- 2.Problem Solving/01.Warmup/004.A VeryBigSum.py
+++ 2.Problem Solving/01.Warmup/004.A VeryBigSum.py
@@ -1,5 +1,6 @@
 '''
-Question : In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+Question : In this challenge, you are required to calculate and print the sum of the elements in an array,
+           keeping in mind that some of those integers may be quite large.
 
 Link : https://www.hackerrank.com/challenges/a-very-big-sum/problem
 '''
