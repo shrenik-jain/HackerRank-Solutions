7c6b1c0d327cb98c92637c663b584044388b232b
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-08 18:24:01 +0530

    Update and rename Loops.py to 005.Loops.py

---
+++ 1.Python/1.Introduction/005.Loops.py
@@ -0,0 +1,8 @@
+'''
+Question : The provided code stub reads and integer, n. For all non-negative integers, i, print i^2.
+
+Link : https://www.hackerrank.com/challenges/python-loops/problem
+'''
+
+for i in range(int(input())):
+    print(i**2)
