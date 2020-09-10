a66b3dd3b8d37f5b43a2df737a9e57d04c7e6602
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-10 15:36:01 +0530

    Update 005.Lists.py

--- 1.Python/2.Basic Data Types/005.Lists.py
+++ 1.Python/2.Basic Data Types/005.Lists.py
@@ -1,3 +1,11 @@
+'''
+Question : Initialize your list and read in the value of n followed by n lines of commands where each command 
+           will be of the 7 types listed above i.e insert, print, remove, append, sort, pop, remove. 
+           Iterate through each command in order and perform the corresponding operation on your list.
+ 
+Link : https://www.hackerrank.com/challenges/python-lists/problem
+'''
+
 if __name__=='__main__':
     def commdr(lst, instruct): 
         if instruct[0] == 'insert':
