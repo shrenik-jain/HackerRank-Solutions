e375520ed52feff5a45f8f90e5fb3d9a6b653b60
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-10 15:31:45 +0530

    Update 003.NestedList.py

--- 1.Python/2.Basic Data Types/003.NestedList.py
+++ 1.Python/2.Basic Data Types/003.NestedList.py
@@ -1,3 +1,12 @@
+'''
+Question : Given the names and grades for each student in a class of N students, store them in a nested list and 
+           print the name(s) of any student(s) having the second lowest grade.
+           Note: If there are multiple students with the second lowest grade, order their names alphabetically and 
+           print each name on a new line.
+           
+Link : https://www.hackerrank.com/challenges/nested-list/problem           
+'''
+
 marksheet=[]
 scorelist=[]
 if __name__ == '__main__':
