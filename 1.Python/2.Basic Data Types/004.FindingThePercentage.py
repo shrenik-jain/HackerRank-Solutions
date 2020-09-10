ff20397a5a946616e9dede0020cc5410632c67da
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-10 15:33:10 +0530

    Update 004.FindingThePercentage.py

--- 1.Python/2.Basic Data Types/004.FindingThePercentage.py
+++ 1.Python/2.Basic Data Types/004.FindingThePercentage.py
@@ -1,3 +1,10 @@
+'''
+Question : The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. 
+           Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+Link : https://www.hackerrank.com/challenges/finding-the-percentage/problem
+'''
+
 if __name__ == '__main__':
     n = int(input())
     student_marks = {}
