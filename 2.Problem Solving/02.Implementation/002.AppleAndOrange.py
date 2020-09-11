50a58b8177a2ed31971d5b7a3b6c3f378b0cdf6e
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-11 11:37:52 +0530

    Update 002.AppleAndOrange.py

--- 2.Problem Solving/02.Implementation/002.AppleAndOrange.py
+++ 2.Problem Solving/02.Implementation/002.AppleAndOrange.py
@@ -1,8 +1,16 @@
-import math
-import os
-import random
-import re
-import sys
+'''
+Question : Sam's house has an apple tree and an orange tree that yield an abundance of fruit. 
+           The red region denotes his house, where s is the start point, and t is the endpoint. 
+           The apple tree is to the left of his house, and the orange tree is to its right. 
+           You can assume the trees are located on a single point, where the apple tree is at point a, 
+           and the orange tree is at point b.
+           When a fruit falls from its tree, it lands d units of distance from its tree of origin along the x-axis. 
+           A negative value of d means the fruit fell d units to the tree's left, and a positive value of d means 
+           it falls d units to the tree's right. Given the value of d for m apples and n oranges, 
+           determine how many apples and oranges will fall on Sam's house (i.e. in the inclusive range [s,t])?
+
+Link : https://www.hackerrank.com/challenges/apple-and-orange/problem
+'''
 
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     num_a , num_o = 0 , 0
@@ -19,22 +27,17 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
     print(num_a , num_o , sep='\n')
 
 if __name__ == '__main__':
+    
     st = input().split()
-
     s = int(st[0])
-
     t = int(st[1])
 
     ab = input().split()
-
     a = int(ab[0])
-
     b = int(ab[1])
 
     mn = input().split()
-
     m = int(mn[0])
-
     n = int(mn[1])
 
     apples = list(map(int, input().rstrip().split()))
