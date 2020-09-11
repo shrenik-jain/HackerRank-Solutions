8159bd39ca1e3ec888615583370e78cd73322b3b
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-11 11:47:37 +0530

    Update 004.BreakingTheRecords.py

--- 2.Problem Solving/02.Implementation/004.BreakingTheRecords.py
+++ 2.Problem Solving/02.Implementation/004.BreakingTheRecords.py
@@ -1,8 +1,12 @@
-import math
-import os
-import random
-import re
-import sys
+'''
+Question : Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. 
+           She tabulates the number of times she breaks her season record for most points and least points in a game. 
+           Points scored in the first game establish her record for the season, and she begins counting from there.
+           Given Maria's scores for a season, find and print the number of times she breaks her records for most and 
+           least points scored during the season.
+           
+Link : https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+'''
 
 def breakingRecords(scores):
     highest  , lowest = 0 , 0
@@ -19,7 +23,6 @@ def breakingRecords(scores):
     return [highest ,lowest]
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input())
 
@@ -27,7 +30,4 @@ if __name__ == '__main__':
 
     result = breakingRecords(scores)
 
-    fptr.write(' '.join(map(str, result)))
-    fptr.write('\n')
-
-    fptr.close()
+    print(' '.join(map(str, result)))
