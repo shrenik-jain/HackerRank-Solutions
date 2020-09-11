468b37151c579f99fc5a963723393ef41ce02e02
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-11 11:53:51 +0530

    Update 006.SockMerchant.py

--- 2.Problem Solving/02.Implementation/006.SockMerchant.py
+++ 2.Problem Solving/02.Implementation/006.SockMerchant.py
@@ -1,22 +1,25 @@
-import math
-import os
-import random
-import re
-import sys
+'''
+Question : John works at a clothing store. He has a large pile of socks that he must pair by color for sale. 
+           Given an array of integers representing the color of each sock, determine how many pairs of socks 
+           with matching colors there are.
+
+Link : https://www.hackerrank.com/challenges/sock-merchant/problem
+'''
 
 def sockMerchant(n, ar):   
     pair , i = 0,0
     ar.sort()
+    
     while i<(n-1):
         if ar[i] == ar[i+1]:
             pair = pair+1
             i=i+2
         else:
             i=i+1
+            
     return pair
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input())
 
@@ -24,6 +27,4 @@ if __name__ == '__main__':
 
     result = sockMerchant(n, ar)
 
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    print(result)
