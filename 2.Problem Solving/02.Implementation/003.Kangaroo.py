f8bf2f18e10b66dbfe58ef8497a49f07d4484209
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-11 11:42:21 +0530

    Update 003.Kangaroo.py

--- 2.Problem Solving/02.Implementation/003.Kangaroo.py
+++ 2.Problem Solving/02.Implementation/003.Kangaroo.py
@@ -1,27 +1,25 @@
-import math
-import os
-import random
-import re
-import sys
+'''
+Question : You are choreographing a circus show with various animals. 
+           For one act, you are given two kangaroos on a number line ready to jump in the positive direction.
+           1.The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
+           2.The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
+           You have to figure out a way to get both kangaroos at the same location at the same time as part of the show. 
+           If it is possible, return YES, otherwise return NO.
+
+Link : https://www.hackerrank.com/challenges/kangaroo/problem
+'''
 
 def kangaroo(x1, v1, x2, v2):
-    return 'YES' if (v1>v2) and (x1-x2)%(v2-v1)==0 else 'NO'
+    return 'YES' if (v1 > v2) and (x1 - x2) % (v2 - v1) == 0 else 'NO'
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     x1V1X2V2 = input().split()
-
     x1 = int(x1V1X2V2[0])
-
     v1 = int(x1V1X2V2[1])
-
     x2 = int(x1V1X2V2[2])
-
     v2 = int(x1V1X2V2[3])
 
     result = kangaroo(x1, v1, x2, v2)
-
-    fptr.write(result + '\n')
-
-    fptr.close()
+    
+    print(result)
