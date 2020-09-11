7b80644f6cc199f364935aa2217a8f605a223d73
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-11 11:50:23 +0530

    Update 005.BirthdayChocolate.py

--- 2.Problem Solving/02.Implementation/005.BirthdayChocolate.py
+++ 2.Problem Solving/02.Implementation/005.BirthdayChocolate.py
@@ -1,7 +1,11 @@
-import os
-import random
-import re
-import sys
+'''
+Question : Lily has a chocolate bar that she wants to share it with Ron for his birthday. 
+           Each of the squares has an integer on it. She decides to share a contiguous segment of the bar selected such that 
+           the length of the segment matches Ron's birth month and the sum of the integers on the squares is equal to his birth day. 
+           You must determine how many ways she can divide the chocolate.
+
+Link : https://www.hackerrank.com/challenges/the-birthday-bar/problem
+'''
 
 def birthday(s, d, m):
     tp = (len(s)-m) + 1 
@@ -9,20 +13,15 @@ def birthday(s, d, m):
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input().strip())
 
     s = list(map(int, input().rstrip().split()))
 
     dm = input().rstrip().split()
-
     d = int(dm[0])
-
     m = int(dm[1])
 
     result = birthday(s, d, m)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    
+    print(result)
