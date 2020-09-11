4679b320112551003a1735ca0d02a1258d32cc4a
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-11 12:19:58 +0530

    Update 001.FibonacciModified.py

--- 2.Problem Solving/03.Dynamic Programming/001.FibonacciModified.py
+++ 2.Problem Solving/03.Dynamic Programming/001.FibonacciModified.py
@@ -1,8 +1,8 @@
-import math
-import os
-import random
-import re
-import sys
+'''
+Question : Given three integers, t1, t2, and n, compute and print the nth term of a modified Fibonacci sequence.
+
+Link : https://www.hackerrank.com/challenges/fibonacci-modified/problem
+'''
 
 def fibonacciModified(t1, t2, n , memo):
     if n<=2:
@@ -15,20 +15,14 @@ def fibonacciModified(t1, t2, n , memo):
     return memo[-1]
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     t1T2n = input().split()
-
     t1 = int(t1T2n[0])
-
     t2 = int(t1T2n[1])
-
     n = int(t1T2n[2])
 
     memo = [t1,t2]
 
     result = fibonacciModified(t1, t2, n, memo)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    
+    print(result)
