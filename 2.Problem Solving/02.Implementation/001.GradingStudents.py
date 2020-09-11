e4630398e28e615b15c325617a093037e08eee48
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-11 11:29:16 +0530

    Update 001.GradingStudents.py

--- 2.Problem Solving/02.Implementation/001.GradingStudents.py
+++ 2.Problem Solving/02.Implementation/001.GradingStudents.py
@@ -1,8 +1,10 @@
-import math
-import os
-import random
-import re
-import sys
+'''
+Question : Sam is a professor at the HackerLand University and likes to round each student's grade according to these rules:
+           1.If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+           2.If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+
+Link : https://www.hackerrank.com/challenges/grading/problem
+'''
 
 def gradingStudents(grades):
     # Write your code here
