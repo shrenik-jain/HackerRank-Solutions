d66063009a20191349aada6e63864ddf1a44c34e
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-17 17:56:30 +0530

    Update 006.StringValidators.py

--- 1.Python/03.Strings/006.StringValidators.py
+++ 1.Python/03.Strings/006.StringValidators.py
@@ -1,3 +1,10 @@
+'''
+Question : You are given a string S. Your task is to find out if the string S contains: 
+           alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+
+Link : https://www.hackerrank.com/challenges/string-validators/problem
+'''
+
 if __name__ == '__main__':
     str = input()
     for i in ('isalnum' , 'isalpha' , 'isdigit','islower', 'isupper'):
