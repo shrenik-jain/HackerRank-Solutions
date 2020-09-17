4d00e4e3acd7e7ea5584db9b87cbe4a3a9da8564
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-17 17:50:30 +0530

    Update 003.What'sYourName.py

--- 1.Python/03.Strings/003.What'sYourName.py
+++ 1.Python/03.Strings/003.What'sYourName.py
@@ -1,3 +1,11 @@
+'''
+Question : You are given the firstname and lastname of a person on two different lines. 
+           Your task is to read them and print the following:
+           "Hello firstname lastname! You just delved into python."
+
+Link : https://www.hackerrank.com/challenges/whats-your-name/problem
+'''
+
 def print_full_name(a, b):
-    print("Hello",a,b,end="")
+    print("Hello", a , b , end="")
     print("! You just delved into python.")
