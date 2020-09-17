9a760544a87fcecc2c77a92cd94315c3bffff849
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-17 17:54:32 +0530

    Update 005.FindAString.py

--- 1.Python/03.Strings/005.FindAString.py
+++ 1.Python/03.Strings/005.FindAString.py
@@ -1,6 +1,15 @@
+'''
+Question : In this challenge, the user enters a string and a substring. 
+           You have to print the number of times that the substring occurs in the given string. 
+           String traversal will take place from left to right, not from right to left.
+           NOTE: String letters are case-sensitive.
+
+Link : https://www.hackerrank.com/challenges/find-a-string/problem
+'''
+
 def count_substring(string, sub_string):
-    n=0
-    for i in range(0,len(string)):
-        if string[i:len(sub_string)+i]==sub_string:
-            n+=1          
+    n = 0
+    for i in range(0 , len(string)):
+        if string[i : len(sub_string) + i] == sub_string:
+            n += 1          
     return n
