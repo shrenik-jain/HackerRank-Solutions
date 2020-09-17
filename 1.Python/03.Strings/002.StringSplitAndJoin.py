d3e16ddda15645e6e1c5248010cbf48ed98473d3
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-17 17:48:58 +0530

    Update 002.StringSplitAndJoin.py

--- 1.Python/03.Strings/002.StringSplitAndJoin.py
+++ 1.Python/03.Strings/002.StringSplitAndJoin.py
@@ -1,4 +1,10 @@
+'''
+Question : You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+
+Link : https://www.hackerrank.com/challenges/python-string-split-and-join/problem
+'''
+
 def split_and_join(line):
-    line=line.split(" ")
-    line="-".join(line)
+    line = line.split(" ")
+    line = "-".join(line)
     return line
