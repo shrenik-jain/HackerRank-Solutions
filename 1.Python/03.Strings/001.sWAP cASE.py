de48069d5c76cf671baa4a3dd9383c2898448f0c
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-17 17:47:50 +0530

    Update 001.sWAP cASE.py

--- 1.Python/03.Strings/001.sWAP cASE.py
+++ 1.Python/03.Strings/001.sWAP cASE.py
@@ -1,2 +1,9 @@
+'''
+Question : You are given a string and your task is to swap cases. 
+           In other words, convert all lowercase letters to uppercase letters and vice versa.
+
+Link : https://www.hackerrank.com/challenges/swap-case/problem
+'''
+
 def swap_case(s):
     return s.swapcase()
