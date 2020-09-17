7cc482b04796e5492114d5be5b489cdef35ca736
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-17 17:52:36 +0530

    Update 004.Mutations.py

--- 1.Python/03.Strings/004.Mutations.py
+++ 1.Python/03.Strings/004.Mutations.py
@@ -1,2 +1,8 @@
+'''
+Question : Read a given string, change the character at a given index and then print the modified string.
+
+Link : https://www.hackerrank.com/challenges/python-mutations/problem
+ '''
+
 def mutate_string(string, position, character):
-    return string[:position]+character+string[position+1:]
+    return string[: position] + character + string[position+1 :]
