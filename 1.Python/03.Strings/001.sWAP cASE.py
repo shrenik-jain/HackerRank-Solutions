f223e6e433a3f8d9168eda76c6e92b7250c432dc
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-18 17:25:28 +0530

    Update 001.sWAP cASE.py

--- 1.Python/03.Strings/001.sWAP cASE.py
+++ 1.Python/03.Strings/001.sWAP cASE.py
@@ -7,3 +7,8 @@ Link : https://www.hackerrank.com/challenges/swap-case/problem
 
 def swap_case(s):
     return s.swapcase()
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
