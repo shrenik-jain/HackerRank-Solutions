d968664dbd267bbb20a7d9e41916e3b2e9c56160
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-21 14:02:38 +0530

    Updated the scripts with a few changes

--- 1.Python/03.Strings/014.MergeTheTools!.py
+++ 1.Python/03.Strings/014.MergeTheTools!.py
@@ -1,9 +1,13 @@
 from collections import OrderedDict
+
 def merge_the_tools(string, k):
     to=list()
-    for i in range(0,len(string),k):
-        to.append(string[i:i+k])
+    for i in range(0 , len(string) , k):
+        to.append(string[i : i+k])
 
     for w in to:
         print("".join(OrderedDict.fromkeys(w)))
 
+f __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
