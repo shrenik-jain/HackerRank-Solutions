3dad11f0996d765b648bd9f8dbdaa199c4130345
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-21 14:01:28 +0530

    Updated the script

--- 1.Python/03.Strings/013.TheMinionGame.py
+++ 1.Python/03.Strings/013.TheMinionGame.py
@@ -1,15 +1,20 @@
 def minion_game(string):
-    p1,p2=0,0
-    n=len(string)
+    p1 , p2 = 0 , 0
+    n = len(string)
+    
     for i in range(n):
         if string[i] in "AEIOU":
-            p1+=n-i
+            p1 += n - i
         else:
-            p2+=n-i
-    if p1>p2:
-        print("Kevin",p1)
-    elif p2>p1:
-        print("Stuart",p2)
+            p2 += n - i
+            
+    if p1 > p2:
+        print("Kevin" , p1)
+    elif p2 > p1:
+        print("Stuart" , p2)
     else:
         print("Draw")
              
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
