47acfcf2d2595da86d86cc3d018f60ed9be6e385
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-21 11:29:27 +0530

    Made a few changes to the script

---
+++ 1.Python/04.Sets/008.SetDifference.py
@@ -0,0 +1,16 @@
+'''
+Question : Students of District College have a subscription to English and French newspapers. 
+           Some students have subscribed to only the English newspaper, some have subscribed to only the 
+           French newspaper, and some have subscribed to both newspapers. You are given two sets of student roll numbers. 
+           One set has subscribed to the English newspaper, and one set has subscribed to the French newspaper. 
+           Your task is to find the total number of students who have subscribed to only English newspapers.
+
+Link : https://www.hackerrank.com/challenges/py-set-difference-operation/problem
+'''
+
+_ = int(input())
+english = set(map(int ,input().split()))
+_ = int(input())
+french = set(map(int ,input().split()))
+
+print(len(english.difference(french)))
