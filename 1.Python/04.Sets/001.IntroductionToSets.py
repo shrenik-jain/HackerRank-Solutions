9ff4258822ea41246f841163f73cd89ff6a37176
Shrenik Jain <shrenikkjain81@gmail.com>
2020-09-21 18:29:14 +0530

    Minor changes

--- 1.Python/04.Sets/001.IntroductionToSets.py
+++ 1.Python/04.Sets/001.IntroductionToSets.py
@@ -9,3 +9,9 @@ Link : https://www.hackerrank.com/challenges/py-introduction-to-sets/problem
 def average(array):
     s = set(array)
     return sum(s)/len(s)
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
