302b3d0d90cc40b54b64ecc86f152849dde3e07a
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-23 13:41:29 +0530

    Made minor changes to script

--- 1.Python/05.Math/004.ModDivmod.py
+++ 1.Python/05.Math/004.ModDivmod.py
@@ -1,7 +1,15 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-if __name__=='__main__':
-    a=int(input())
-    b=int(input())
-    print(a//b)
-    print(a%b)
-    print(divmod(a,b))
+'''
+Question : Read in two integers, a and b, and print three lines.
+           The first line is the integer division a//b.
+           The second line is the result of the modulo operator a%b.
+           The third line prints the divmod of a and b.
+
+Link : https://www.hackerrank.com/challenges/python-mod-divmod/problem
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a // b)
+    print(a % b)
+    print(divmod(a , b))
