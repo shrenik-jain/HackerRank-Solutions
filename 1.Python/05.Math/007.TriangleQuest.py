8cb615a33ec87b6257a34ad21415ee4903fa1c83
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-23 13:46:24 +0530

    Update 007.TriangleQuest.py

--- 1.Python/05.Math/007.TriangleQuest.py
+++ 1.Python/05.Math/007.TriangleQuest.py
@@ -1,2 +1,14 @@
-for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+'''
+Question : You are given a positive integer N. Print a numerical triangle of height N-1 like the one below:
+           1
+           22
+           333
+           4444
+           55555
+           ......
+
+Link : https://www.hackerrank.com/challenges/python-quest-1/problem
+'''
+
+for i in range(1,int(input())): 
     print(((10**i)//9)*i)
