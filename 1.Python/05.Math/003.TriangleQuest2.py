a680dd7c92b36ba59025262ee489d660dd11f089
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-23 13:38:47 +0530

    Update 003.TriangleQuest2.py

--- 1.Python/05.Math/003.TriangleQuest2.py
+++ 1.Python/05.Math/003.TriangleQuest2.py
@@ -1,2 +1,15 @@
-for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+'''
+Question : You are given a positive integer N.
+           Your task is to print a palindromic triangle of size N.
+           For example, a palindromic triangle of size  is:
+           1
+           121
+           12321
+           1234321
+           123454321
+
+Link : https://www.hackerrank.com/challenges/triangle-quest-2/problem
+'''
+
+for i in range(1,int(input())+1): 
     print (((10**i)//9)**2)
