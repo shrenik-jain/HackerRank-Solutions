5881594f7527d4f795be25371ec1d3674f48c44b
Shrenik Jain <50694291+shrenik-jain@users.noreply.github.com>
2020-09-23 13:44:37 +0530

    Minor changes were made to the script

--- 1.Python/05.Math/006.IntegersComeInAllSizes.py
+++ 1.Python/05.Math/006.IntegersComeInAllSizes.py
@@ -1,7 +1,11 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-a=int(input())
-b=int(input())
-c=int(input())
-d=int(input())
-print(pow(a,b)+pow(c,d))
+'''
+Question : Read four numbers, a, b, c, and d, and print the result of a^b + c^d.
 
+Link : https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem 
+'''
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(pow(a , b) + pow(c , d))
